4d39d6151be57d364f2c1a70586d34c7eeb65f7d
akshatV21 <a.vishwakarma2110@gmail.com>
2022-09-30 16:52:06 +0530

    updated

--- src/api/utils/getToken.js
+++ src/api/utils/getToken.js
@@ -2,7 +2,7 @@ const EntityModel = require("../models/entity.model")
 
 const getToken = async () => {
   const lastEntity = await EntityModel.findOne().sort({ createdAt: -1 })
-  return lastEntity ? lastEntity.token + 1 : 10001
+  return lastEntity ? lastEntity.token + 1 : 10000
 }
 
 module.exports = getToken
