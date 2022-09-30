0d9dca98a59508146303bd939fbd04eb5fcd7abc
akshatV21 <a.vishwakarma2110@gmail.com>
2022-09-30 16:39:18 +0530

    updated

--- src/api/utils/generatePdf.js
+++ src/api/utils/generatePdf.js
@@ -1,7 +1,7 @@
 const puppeteer = require("puppeteer")
 
 const generatePdf = async id => {
-  const browser = await puppeteer.launch({ headless: true })
+  const browser = await puppeteer.launch({ headless: true, args: ["--no-sandbox"] })
   const page = await browser.newPage()
   await page.goto(`https://stickman-assessment.herokuapp.com/pdf/${id}`)
   const pdf = await page.pdf({ format: "A4" })
